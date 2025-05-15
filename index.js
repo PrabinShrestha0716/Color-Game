6cbb6b0c983f23a20c8adf963970434574fdb224
Prabin2023 <shrestha.prabin2000@gmail.com>
2025-05-15 13:14:18 -0500

    updated

--- index.js
+++ index.js
@@ -18,34 +18,36 @@ $(document).on("keypress",function ()
   $("h1").text("Game Started: Score: "+ score)
 });
 
-function nextSequence(){
-random_number = Math.floor (Math.random() * 3);
-randomChosenColour = buttonColors[random_number];
-gamePattern= randomChosenColour;
+function nextSequence()
+{
+  random_number = Math.floor (Math.random() * 3);
+  randomChosenColour = buttonColors[random_number];
+  gamePattern= randomChosenColour;
 
-$("#" + randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
-playSound(randomChosenColour);
+  $("#" + randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+  playSound(randomChosenColour);
 }
 
 
-$(".btn").on("click", function (){
+$(".btn").on("click", function ()
+{
   $(this).animate({opacity:"50%"}).animate({opacity:"100%"});
   $("h1").fadeOut(200).fadeIn(200);
-var userChosenColour = $(this).attr("id");
-//  userPattern.push(userChosenColour);
- playSound(userChosenColour);
+  var userChosenColour = $(this).attr("id");
+  playSound(userChosenColour);
 
  if (gamePattern==userChosenColour) 
-        {
-          score = score+1;
-         $("h1").text("Game started. Score: "+ score); 
-             nextSequence(); 
-        } 
+  {
+    score = score+1;
+    $("h1").text("Game started. Score: "+ score); 
+        nextSequence(); 
+  } 
  
 
 })
 
-function playSound(name){
+function playSound(name)
+{
    var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
   audio.play();
 }
